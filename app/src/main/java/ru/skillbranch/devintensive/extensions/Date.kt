d1d150318a424953067b3002537fa9c0bb43205a
TemichCh <temich.42.ch@gmail.com>
2019-07-03 16:21:38 +0700

    time to the future

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -3,6 +3,7 @@ package ru.skillbranch.devintensive.extensions
 import ru.skillbranch.devintensive.utils.Utils
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
 const val SECOND = 1000
 const val MINUTE = 60 * SECOND
@@ -42,10 +43,11 @@ fun Date.humanizeDiff(date: Date = Date()): String {
     val t = thisTime - anotherTime
     println("${date.format()} , ${this.format()}")
     println("$thisTime , $anotherTime, $t")
+    if (t>0)
     when (t) {
-        in (0L* SECOND..1L*SECOND) -> return "только что"
-        in (1L*SECOND..45L*SECOND) -> return "несколько секунд назад"
-        in (75L*SECOND..45L * MINUTE) -> return "${t / MINUTE} ${Utils.days_to_string(
+        in (0L * SECOND..1L * SECOND) -> return "только что"
+        in (1L * SECOND..45L * SECOND) -> return "несколько секунд назад"
+        in (75L * SECOND..45L * MINUTE) -> return "${t / MINUTE} ${Utils.days_to_string(
             t / MINUTE,
             "минуту",
             "минуты",
@@ -64,6 +66,30 @@ fun Date.humanizeDiff(date: Date = Date()): String {
         }
         else -> return "более года назад"
     }
+    else
+        when (t.absoluteValue) {
+            in (0L * SECOND..1L * SECOND) -> return "только что"
+            in (1L * SECOND..45L * SECOND) -> return "через несколько секунд"
+            in (75L * SECOND..45L * MINUTE) -> return "через ${t.absoluteValue / MINUTE} ${Utils.days_to_string(
+                t.absoluteValue / MINUTE,
+                "минуту",
+                "минуты",
+                "минут"
+            )}"
+            in (45L * MINUTE..75L * MINUTE) -> return "через час"
+            in (75L * MINUTE..22L * HOUR) -> return "через ${t.absoluteValue / HOUR} ${Utils.days_to_string(
+                t.absoluteValue / HOUR,
+                "час",
+                "часа",
+                "часов"
+            )}"
+            in (22L * HOUR..26L * HOUR) -> return "через день"
+            in (26L * HOUR..360L * DAY) -> {
+                return "через ${t.absoluteValue / DAY} ${Utils.days_to_string(t.absoluteValue / DAY, "день", "дня", "дней")}"
+            }
+            else -> return "более года"
+        }
+
 }
 
 
